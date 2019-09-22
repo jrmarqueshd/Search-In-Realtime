04414a08302608699181a3a50d6ee96859d8a3d8
Junior <gilmar_junior@hotmaill.com.br>
2019-09-22 19:39:36 -0300

    include verification regex on application

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# simple-search-field
+# Getting Starter
+
+Embed in your HTML before the end `</body>` tag
+```
+<script src=""></script>
+```
+
+Define in your element `<input />`
+```
+<input search-field="on" type="search" />
+```
+
+Define in your element `<ul>` tag
+```
+<ul list-search-id="on">
+```
+
+**And ready, the magic will happen!**
+
+<br/>
+<br/>
+<br/>
+
+*NOTE: Elements within `<ul>` tag must be of type `<li>` for the best working*
--- public/main.js
+++ public/main.js
@@ -1,24 +1,31 @@
 if(document.querySelector('input[type="search"').getAttribute("search-field") == "on"){
     const $searchInput = document.querySelector("input");
+    const $head = document.querySelector("html > head");
+    const styledCustimized = `<style> ul > li[custom-class="hidden"]{display: none!important;}</style>`;
 
-    if(document.getElementById("listToSearch")){
-        const $listToSearch = document.getElementById("listToSearch");
+    $head.insertAdjacentHTML("beforeend", styledCustimized);
+    if(document.querySelector('ul[list-search-id="on"]')){
+        const $listToSearch = document.querySelector('ul[list-search-id="on"]');
         let $childrenListSearch = $listToSearch.children;
-
         $searchInput.addEventListener("keyup", ()=>{
-            [].forEach.call($childrenListSearch, (each)=>{
-                if($searchInput.value == each.innerHTML){
-                    each.style.display = "inherit";
-                }else{
-                    each.style.display = "none";
-                }
-            });
-        })
-        
+            let verifyInputValue = $searchInput.value;
+            if($searchInput.value.length){
+                [].forEach.call($childrenListSearch, (eachChildren)=>{
+                    if(RegExp(verifyInputValue, "i").test(eachChildren.innerHTML)){
+                        eachChildren.removeAttribute("custom-class");
+                    }else{
+                        eachChildren.setAttribute("custom-class", "hidden");
+                    }
+                });
+            }else{
+                [].forEach.call($childrenListSearch, (eachChildren)=>{
+                    eachChildren.removeAttribute("custom-class");
+                });
+            }
+        });
     }else{
-        console.log('Você precisa definir uma <ul id="listToSearch">');
+        console.log('Você precisa definir uma <ul list-search-id="on">');
     }
-
 }else{
     console.log('Você precisa definir <input search-field="on" type="search" />')
 }
