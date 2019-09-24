bfc0169496aadef47c920f3d0ad4ad964168d5e9
Junior <gilmar_junior@hotmaill.com.br>
2019-09-23 21:08:23 -0300

    updates

--- public/script.js
+++ public/script.js
@@ -1,12 +1,10 @@
 window.addEventListener("load", ()=>{
     document.querySelector(".lds-circle").classList.add("off");
 
-    // Variaveis JS
     let date = new Date();
     let year = date.getUTCFullYear(-3);
     let since = 2019;
 
-    // Variaveis Browser
     let $year = document.getElementById("year");
     $year.innerText = since==year ? `${year}` : `${since} - ${year}`;
 })
